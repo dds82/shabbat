5a2b62f081f6195600138d0b6a5a4b2702d5b4aa
dds82 <47287470+dds82@users.noreply.github.com>
2021-07-22 15:37:36 -0400

    Update hubitat-shabbat-scheduler.groovy
    added speak text attribute

--- hubitat-shabbat-scheduler.groovy
+++ hubitat-shabbat-scheduler.groovy
@@ -47,6 +47,7 @@ metadata {
         attribute "activeType", "enum", ["Regular", "Plag", "Early"]
         attribute "havdalah", "enum", ["None", "Fire", "No Fire"]
         attribute "specialHoliday", "enum", ["", PESACH, SHAVUOT, SUKKOT, YOM_KIPPUR]
+        attribute "candleLightingSpeakText", "string"
         command "regular"
         command "plag"
         command "early"
@@ -97,8 +98,9 @@ def createStateMap() {
 }
  
  def initialize() {
-     unschedule(fetchSchedule)
+     doUnschedule()
      createStateMap()
+     sendEvent("name": "candleLightingSpeakText", "value": "")
      
      schedulePendingEvent()
     fetchSchedule()
@@ -119,8 +121,15 @@ def updated() {
  }
 
 def uninstalled() {
-    unschedule(fetchSchedule)
+    doUnschedule()
     eventLists.remove(device.id)
+}
+
+def doUnschedule() {
+    unschedule(fetchSchedule)
+    unschedule(updateSpeakText)
+    unschedule(shabbatStart)
+    unschedule(shabbatEnd)
 }
 
 def testEvent(String eventType, BigDecimal delay) {
@@ -379,8 +388,25 @@ def schedulePendingEvent() {
             log.debug "next event is ${state.nextEventType} at ${nextEventTime}, holiday=${state.specialHoliday} with extraData=${extraData}"
         }
         
-        if (state.nextEventType == CANDLES)
+        if (state.nextEventType == CANDLES) {
             schedule(scheduleStr, shabbatStart, [data: extraData])
+            
+            if (!state.nextEventTest) {
+                Calendar speakTime = Calendar.getInstance()
+                speakTime.setTime(nextEventTime)
+                speakTime.set(Calendar.SECOND, 0)
+                speakTime.set(Calendar.MINUTE, 0)
+                speakTime.set(Calendar.HOUR, 0)
+                if (now() >= speakTime.getTimeInMillis()) {
+                    updateSpeakText()
+                }
+                else {
+                    String textUpdateSchedule = String.format("%d %d %d %d %d ? %d", 0, 0, 0, cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.YEAR))
+                    extraData["when"] = nextEventTime
+                    schedule(textUpdateSchedule, updateSpeakText, [data: extraData])
+                }
+            }
+        }
         else
             schedule(scheduleStr, shabbatEnd, [data: extraData])
     }
@@ -397,6 +423,12 @@ def schedulePendingEvent() {
         // Re-fetch
         fetchSchedule()
     }
+}
+
+def updateSpeakText(data) {
+    SimpleDateFormat sdf = new SimpleDateFormat("h:mm a")
+    String text = String.format("Candle lighting today is at %s", sdf.format(data["when"]))
+    sendEvent("name": "candleLightingSpeakText", "value": text)
 }
 
 def shabbatStart(data) {
@@ -475,6 +507,7 @@ def shabbatEnd(data) {
     state.specialHoliday = nextSpecialHoliday
     sendEvent("name": "specialHoliday", "value": (nextSpecialHoliday == null ? "" : nextSpecialHoliday))
     sendEvent("name": "havdalah", "value": aish)
+    sendEvent("name": "candleLightingSpeakText", "value": "")
     if (preferEarly)
         restorePreviousManualType()
     
